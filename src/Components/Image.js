9352f776af0150c34adb3719466cfd1710912e42
ShevDenny <sdenny904@gmail.com>
2021-06-22 22:09:42 -0400

    updated

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,8 +1,16 @@
+import { useState } from "react"
+
 function Image({ url,mapAPI,ID }) {
+    const [favorite, setFavorite] = useState(false)
+
+    function handleClick () {
+        setFavorite(!favorite)
+    }
+
     return (
         <div className = "card" id = {`item_${ID}`}>
             <img src = {`https://maps.googleapis.com/maps/api/place/photo?maxwidth=10000&photoreference=${url}&key=${mapAPI}`}/>
-            <button className="fave">☆</button>
+            <button className={!favorite ? "fave" : "favored" } onClick={handleClick}> {!favorite ? "☆" : "★" }</button>
         </div>
     )
 }
